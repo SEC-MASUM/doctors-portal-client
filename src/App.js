51cd31792ac9e37e43396cd290988976d706f5d2
SEC-MASUM <masum.jess42@gmail.com>
2022-05-14 07:09:34 +0600

    feat : use ProtectedRoute for Appointment page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Appointment from "./Pages/Appointment/Appointment";
 import Contact from "./Pages/Contact/Contact";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
+import ProtectedRoute from "./Pages/Login/ProtectedRoute";
 import Reviews from "./Pages/Reviews/Reviews";
 import Navbar from "./Pages/Shared/Navbar/Navbar";
 import SignUp from "./Pages/SignUp/SignUp";
@@ -15,7 +16,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/appointment" element={<Appointment />} />
+        <Route
+          path="/appointment"
+          element={
+            <ProtectedRoute>
+              <Appointment />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/reviews" element={<Reviews />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
