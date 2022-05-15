ffed4d3efceae83c27ecc1f8aa5775a63bf17b80
SEC-MASUM <masum.jess42@gmail.com>
2022-05-16 01:41:41 +0600

    refactor : use useQuery from React Query and pass refetch to BookingModal component to automatically update slots

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -2,21 +2,37 @@ import { format } from "date-fns";
 import React, { useEffect, useState } from "react";
 import BookingModal from "../BookingModal/BookingModal";
 import AppointmentService from "./AppointmentService";
+import { useQuery } from "react-query";
+import Loading from "../../Shared/Loading/Loading";
 
 const AvailableAppointments = ({ date }) => {
-  const [appointmentServices, setAppointmentServices] = useState([]);
+  // const [appointmentServices, setAppointmentServices] = useState([]);
   const [treatment, setTreatment] = useState(null);
   const formattedDate = format(date, "PP");
 
-  useEffect(() => {
-    const url = `http://localhost:5000/available?date=${formattedDate}`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        setAppointmentServices(data);
-        // console.log(data);
-      });
-  }, [formattedDate]);
+  const {
+    data: appointmentServices,
+    isLoading,
+    refetch,
+  } = useQuery(["available", formattedDate], () =>
+    fetch(`http://localhost:5000/available?date=${formattedDate}`).then((res) =>
+      res.json()
+    )
+  );
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  // useEffect(() => {
+  //   const url = `http://localhost:5000/available?date=${formattedDate}`;
+  //   fetch(url)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setAppointmentServices(data);
+  //       // console.log(data);
+  //     });
+  // }, [formattedDate]);
 
   return (
     <div className="my-28">
@@ -24,7 +40,7 @@ const AvailableAppointments = ({ date }) => {
         Available Appointments on {format(date, "PP")}
       </h4>
       <div className="grid col-span-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
-        {appointmentServices.map((appointmentService) => (
+        {appointmentServices?.map((appointmentService) => (
           <AppointmentService
             key={appointmentService._id}
             appointmentService={appointmentService}
@@ -37,6 +53,7 @@ const AvailableAppointments = ({ date }) => {
           date={date}
           treatment={treatment}
           setTreatment={setTreatment}
+          refetch={refetch}
         />
       )}
     </div>
