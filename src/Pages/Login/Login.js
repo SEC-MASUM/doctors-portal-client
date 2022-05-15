eb2db06880e6ec0df635d8885f75b70661ef2c02
SEC-MASUM <masum.jess42@gmail.com>
2022-05-15 08:39:30 +0600

    style : add top and bottom margin to make space of the login card

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -48,7 +48,7 @@ const Login = () => {
   };
 
   return (
-    <div className="flex min-h-screen justify-center items-center">
+    <div className="flex min-h-screen justify-center items-center my-10">
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
           <h2 className="text-center text-2xl font-bold">Login</h2>
