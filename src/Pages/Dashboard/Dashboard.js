5f366567697836c9d9cf7ede108a6974c1cdd7e8
SEC-MASUM <masum.jess42@gmail.com>
2022-05-17 00:05:36 +0600

    feat :  change dashboard style and add Reviews and History Link

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -7,19 +7,23 @@ const Dashboard = () => {
       <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
       <div class="drawer-content ">
         {/* <!-- Page content here --> */}
-        <h1 className="text-3xl font-bold text-purple-500">Welcome to your Dashboard</h1>
+        <h1 className="text-3xl font-bold text-purple-500">
+          Welcome to your Dashboard
+        </h1>
         <Outlet></Outlet>
-        
       </div>
       <div class="drawer-side">
         <label for="dashboard-sidebar" class="drawer-overlay"></label>
-        <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+        <ul class="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
           {/* <!-- Sidebar content here --> */}
           <li>
             <Link to="/dashboard">My Appointments</Link>
           </li>
           <li>
-            <Link to="/dashboard/review">My Reviews</Link>
+            <Link to="/dashboard/review">Reviews</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/history">History</Link>
           </li>
         </ul>
       </div>
