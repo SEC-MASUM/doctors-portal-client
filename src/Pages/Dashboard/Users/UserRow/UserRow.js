effe2ebae02215fc3a068a25ea3756fa6ad23cd0
SEC-MASUM <masum.jess42@gmail.com>
2022-05-17 23:55:00 +0600

    feat :  use UserRow component to show user data in a table row there have Make Admin button and Remove User button. Make Admin button is functional

--- src/Pages/Dashboard/Users/UserRow/UserRow.js
+++ src/Pages/Dashboard/Users/UserRow/UserRow.js
@@ -1,13 +1,38 @@
 import React from "react";
-
-const UserRow = ({ user }) => {
-  const { email } = user;
+import { toast } from "react-toastify";
+const UserRow = ({ user, refetch }) => {
+  const { email, role } = user;
+  const makeAdmin = () => {
+    const url = `http://localhost:5000/user/admin/${email}`;
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => {
+        if (res.status === 403) {
+          toast.error("Failed to make an admin");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          refetch(); // to update data automatically in UI
+          toast.success("Successfully made an admin");
+        }
+      });
+  };
   return (
     <tr>
       <th>1</th>
       <td>{email}</td>
       <td>
-        <button class="btn btn-xs">Make Admin</button>
+        {role !== "admin" && (
+          <button onClick={makeAdmin} class="btn btn-xs">
+            Make Admin
+          </button>
+        )}
       </td>
       <td>
         <button class="btn btn-xs">Remove User</button>
