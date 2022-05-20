db7e779193240ee133082b5386ba18e6a8535035
SEC-MASUM <masum.jess42@gmail.com>
2022-05-21 01:53:36 +0600

    change endpoint localhost to heroku

--- src/Pages/Dashboard/Payment/CheckoutForm.js
+++ src/Pages/Dashboard/Payment/CheckoutForm.js
@@ -14,7 +14,7 @@ const CheckoutForm = ({ appointment }) => {
   const { _id, price, patient, patientName } = appointment;
 
   useEffect(() => {
-    const url = `http://localhost:5000/create-payment-intent`;
+    const url = `https://doctors-portal-02.herokuapp.com/create-payment-intent`;
     fetch(url, {
       method: "POST",
       headers: {
@@ -80,7 +80,7 @@ const CheckoutForm = ({ appointment }) => {
         appointment: _id,
         transactionId: paymentIntent.id,
       };
-      fetch(`http://localhost:5000/booking/${_id}`, {
+      fetch(`https://doctors-portal-02.herokuapp.com/booking/${_id}`, {
         method: "PATCH",
         headers: {
           "content-type": "application/json",
